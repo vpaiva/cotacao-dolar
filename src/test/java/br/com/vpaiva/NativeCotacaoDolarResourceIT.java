a6f68a6b7d50a93f99affb605eab0760ba01d2ec
Vinício Paiva <vinicio.paiva@mjv.com.br>
2022-03-04 19:31:47 -0300

    Configuração do projeto.

---
+++ src/test/java/br/com/vpaiva/NativeCotacaoDolarResourceIT.java
@@ -0,0 +1,9 @@
+package br.com.vpaiva;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeCotacaoDolarResourceIT extends CotacaoDolarResourceTest {
+
+    // Execute the same tests but in native mode.
+}
